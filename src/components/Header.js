ac4061700440813dfcff9d27f949aaf5d96d1c5c
Phil Armour <philarmour@users.noreply.github.com>
2018-07-01 16:19:51 -0400

    Launch Day! (#2)
    * initial launch content and style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,30 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import logo from '../assets/images/logo.svg';
+import fhaLogo from '../assets/images/fha-logo.png';
+import fhaLogo2 from '../assets/images/fha-logo.svg';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <span className="logo"><img src={logo} alt="" /></span>
-        <h1>Stellar</h1>
-        <p>Just another free, fully responsive site template<br />
-        design by <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <div>
+            <Link to="/"><img src={logo} alt="Armour Veterinary Ophthalmology" /></Link>
+            <span className="large-span">
+                <h1>Now Open!</h1>
+                <p>Ophthalmology specialist for pets, servicing DC and surrounding areas. Accepting appointements and consults immediately.</p>
+            </span>
+        </div>
+        <div className="smoosh">
+            <span  className="smoosh">
+                <p className="smoosh">Conveniently located at &nbsp;&nbsp;&nbsp;</p>
+            </span>
+            <Link to="/">
+            <span  className="smoosh">
+                <img src={fhaLogo} alt="Friendship Hospital for Animals" />
+            </span>
+            <span className="smoosh"><p>Friendship Hospital for Animals</p></span>
+            </Link>
+        </div>
+
     </header>
 )
 
