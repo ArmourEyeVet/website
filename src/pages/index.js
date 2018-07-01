9dac825debe8599cd3c953e819cf1e64497c68e9
Phil Armour <>
2018-07-01 16:17:02 -0400

    bug fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,7 +53,7 @@ class Index extends React.Component {
                   I enjoy offering the gold standard of medical and surgical care for ophthalmic diseases in our pets. In particular, I enjoy collaboration with human ophthalmologists in the field of corneal diseases, glaucoma and cataracts.
                   Micki Armour VMD, DACVO grew up in the Washington DC area and attended Holton-Arms School in Bethesda, Maryland. When I was growing up in the area, my own pets were patients at Friendship Hospital for Animals.  My husband Phil and I have 3 sons, Brayden, Finn and Eric. I enjoy running, skiing, ice hockey (go Caps!) and being active with the boys. My first-born "children" Brody (a black lab) and Sam (a boxer) love to hike in Great Falls park.
                 </p>
-               <p>
+               <span className="body-text">
                  Commonly treated conditions:
                  <ul>
                    <li>Indolent ulceration</li>
@@ -72,9 +72,11 @@ class Index extends React.Component {
                    <li>Eyelid diseases - entropion/ectropion, ectopic cilia/distichia/trichiass</li>
                    <li>Prolapsed third eyelid gland (“cherry eye”)</li>
                  </ul>
-               </p>
+               </span>
                <p>
                  Armour Veterinary Ophthalmology offers the following diagnostic tests and therapeutic options:
+               </p>
+               <span className="body-text"> 
                  <ul>
                    <li>Schirmer tear test, rebound tonometry, tear film assessment, fluorescein stain</li>
                    <li>Gonioscopy</li>
@@ -86,7 +88,7 @@ class Index extends React.Component {
                    <li>Retinopexy</li>
                    <li>Corneal transplant</li>
                  </ul>
-               </p>
+               </span>
               <p>Appointments are available Monday through Friday 8am-4:30pm
                  Emergent cases can be referred directly or through the Friendship urgent care/ emergency service 
               </p>
@@ -114,7 +116,7 @@ class Index extends React.Component {
               <h3><b>Hours of Operation:</b> M-F 8 a.m - 4:30 p.m., EST</h3>
               <p>Emergent cases can be referred directly or through the Friendship urgent care/emergency service</p>
             </header>
-            <iframe src="http://maps.google.com/maps?q=4105+Brandywine+Street+N.W.,+Washington,+DC+20016&amp;hl=en&amp;ie=UTF8&amp;g=4105+Brandywine+Street+N.W.,+Washington,+DC+20016&amp;s=AARTsJpMOc6f6HuQ12r22IOyA1JP74hwdQ&amp;ll=38.949799,-77.080829&amp;spn=0.005841,0.00912&amp;z=16&amp;iwloc=addr&amp;output=embed" marginWidth="0" marginHeight="0" scrolling="no" width="100%" height="350" frameBorder="0"></iframe>
+            <iframe src="https://maps.google.com/maps?q=4105+Brandywine+Street+N.W.,+Washington,+DC+20016&amp;hl=en&amp;ie=UTF8&amp;g=4105+Brandywine+Street+N.W.,+Washington,+DC+20016&amp;s=AARTsJpMOc6f6HuQ12r22IOyA1JP74hwdQ&amp;ll=38.949799,-77.080829&amp;spn=0.005841,0.00912&amp;z=16&amp;iwloc=addr&amp;output=embed" marginWidth="0" marginHeight="0" scrolling="no" width="100%" height="350" frameBorder="0"></iframe>
             <p>Parking is available off Brandywine Street in our private garage below the building.  Additionally, metered parking is available on surrounding streets and parking garages.  We are happy to reimburse for any costs associated with parking if you bring your receipt to the front desk.</p>
             <br/>
             <h3><b>AVO Direct Phone:</b> (202) 567-2089</h3>
